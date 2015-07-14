b0a197aea5572e27c3abad44e356036fdae633ba
Grizwold <tomekbielaszewski@gmail.com>
2015-07-14 21:01:01 +0200

    Adding new entries works, temporary disabled security

--- src/main/java/org/grizz/keeper/controller/EntryController.java
+++ src/main/java/org/grizz/keeper/controller/EntryController.java
@@ -4,6 +4,8 @@ import org.grizz.keeper.model.Entry;
 import org.grizz.keeper.model.impl.EntryEntity;
 import org.grizz.keeper.model.repos.EntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +29,13 @@ public class EntryController {
                         .value("heheszki")
                         .build());
         return entry;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void add(@RequestBody EntryEntity entry) {
+        if (entry.getDate() == null) {
+            entry.setDate(System.currentTimeMillis());
+        }
+        entryRepository.insert(entry);
     }
 }
--- src/main/java/org/grizz/keeper/model/impl/EntryEntity.java
+++ src/main/java/org/grizz/keeper/model/impl/EntryEntity.java
@@ -1,18 +1,24 @@
 package org.grizz.keeper.model.impl;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
 import org.grizz.keeper.model.Entry;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * Created by Grizz on 2015-07-13.
  */
-@Value
+@Data
 @Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 @Document(collection = "entries")
 public class EntryEntity implements Entry {
     private String key;
     private String value;
     private Long date;
+
+    public EntryEntity() {
+    }
 }
