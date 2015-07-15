2660fb25cd591c34e7e23494bbf44decfb1244ab
tomekbielaszewski <tomekbielaszewski@gmail.com>
2015-07-15 16:28:55 +0200

    API methods added: - Add new entry - list entry history - list last entry

--- src/main/java/org/grizz/keeper/controller/EntryController.java
+++ src/main/java/org/grizz/keeper/controller/EntryController.java
@@ -1,14 +1,12 @@
 package org.grizz.keeper.controller;
 
-import org.grizz.keeper.model.Entry;
 import org.grizz.keeper.model.impl.EntryEntity;
 import org.grizz.keeper.model.repos.EntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by Grizz on 2015-07-13.
@@ -19,15 +17,15 @@ public class EntryController {
     @Autowired
     private EntryRepository entryRepository;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public Entry get() {
-        Entry entry = entryRepository.findAll().stream()
-                .findFirst()
-                .orElse(EntryEntity.builder()
-                        .date(System.currentTimeMillis())
-                        .key("someValue")
-                        .value("heheszki")
-                        .build());
+    @RequestMapping(value = "/{key}", method = RequestMethod.GET)
+    public List<EntryEntity> getHistory(@PathVariable String key) {
+        List<EntryEntity> entries = entryRepository.findByKeyOrderByDateDesc(key);
+        return entries;
+    }
+
+    @RequestMapping(value = "/last/{key}", method = RequestMethod.GET)
+    public EntryEntity getLast(@PathVariable String key) {
+        EntryEntity entry = entryRepository.findTopByKeyOrderByDateDesc(key);
         return entry;
     }
 
--- src/main/java/org/grizz/keeper/model/repos/EntryRepository.java
+++ src/main/java/org/grizz/keeper/model/repos/EntryRepository.java
@@ -3,8 +3,13 @@ package org.grizz.keeper.model.repos;
 import org.grizz.keeper.model.impl.EntryEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 /**
  * Created by Grizz on 2015-07-14.
  */
 public interface EntryRepository extends MongoRepository<EntryEntity, String> {
+    List<EntryEntity> findByKeyOrderByDateDesc(String key);
+
+    EntryEntity findTopByKeyOrderByDateDesc(String key);
 }
