4dd0fc490f423008ebb3420f5828b0f953417db9
Grizwold <tomekbielaszewski@gmail.com>
2015-07-23 08:30:05 +0200

    SpringBoot style MongoDB config

--- src/main/java/org/grizz/keeper/springconfig/BaseConfig.java
+++ src/main/java/org/grizz/keeper/springconfig/BaseConfig.java
@@ -1,12 +1,10 @@
 package org.grizz.keeper.springconfig;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * Created by Grizz on 2015-07-13.
  */
 @Configuration
-@Import(MongoConfig.class)
 public class BaseConfig {
 }
