127d7df196eff0ae23741795ed367164a9039076
Tomasz Bielaszewski <tomasz.bielaszewski@itkontrakt.pl>
2015-08-24 09:32:34 +0200

    Konfiguracja spring security. Gotowe do autoryzacji uzytkownikow z bazy

--- src/main/java/org/grizz/keeper/springconfig/BaseConfig.java
+++ src/main/java/org/grizz/keeper/springconfig/BaseConfig.java
@@ -1,10 +1,12 @@
 package org.grizz.keeper.springconfig;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Created by Grizz on 2015-07-13.
  */
 @Configuration
+@Import(SecurityConfig.class)
 public class BaseConfig {
 }
--- src/main/java/org/grizz/keeper/springconfig/RestAuthenticationProvider.java
+++ src/main/java/org/grizz/keeper/springconfig/RestAuthenticationProvider.java
@@ -1,22 +1,32 @@
 package org.grizz.keeper.springconfig;
 
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Grizz on 2015-07-26.
  */
+@Slf4j
+@Component
 public class RestAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String login = (String) authentication.getPrincipal();
+        String login = authentication.getName();
         String password = (String) authentication.getCredentials();
 
-        //TODO: check authorization here...
+        List<GrantedAuthority> authorities = Lists.newArrayList();
 
-        //..return Spring token..
+        log.info("Logging in: {}, {}, {}", login, password, Arrays.toString(authorities.toArray()));
+
         return new UsernamePasswordAuthenticationToken(login, password, authorities);
     }
 
