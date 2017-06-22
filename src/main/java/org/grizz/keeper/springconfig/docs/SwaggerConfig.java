c725414e4c16a2d12172e7ec5c3d8d6b8915843e
Grizwold <tomekbielaszewski@gmail.com>
2017-06-22 20:02:08 +0200

    swagger, dependencies update, ssl, gitignore

---
+++ src/main/java/org/grizz/keeper/springconfig/docs/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package org.grizz.keeper.springconfig.docs;
+
+import com.google.common.collect.Lists;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Lists.newArrayList(securitySchema()))
+                .securityContexts(Lists.newArrayList(securityContext()));
+    }
+
+    private SecurityScheme securitySchema() {
+        return new BasicAuth("login");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .forPaths(PathSelectors.any())
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("login", authorizationScopes));
+    }
+}
