6deb3636afc963c469140ad05cf9518db2b464a4
Grizwold <tomekbielaszewski@gmail.com>
2017-09-28 21:54:12 +0200

    request logging

--- src/main/java/org/grizz/keeper/springconfig/BaseConfig.java
+++ src/main/java/org/grizz/keeper/springconfig/BaseConfig.java
@@ -1,9 +1,11 @@
 package org.grizz.keeper.springconfig;
 
 import org.grizz.keeper.springconfig.security.SecurityConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 @Configuration
@@ -14,5 +16,13 @@ public class BaseConfig {
         public JsonpAdvice() {
             super("callback");
         }
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        return loggingFilter;
     }
 }
