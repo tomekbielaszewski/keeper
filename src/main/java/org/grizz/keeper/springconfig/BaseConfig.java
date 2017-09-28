fcbf5613f81f569395284cae5b70fa29dbf11076
Grizwold <tomekbielaszewski@gmail.com>
2017-09-28 23:18:42 +0200

    Improved request logging, swagger docs link on landing page

--- src/main/java/org/grizz/keeper/springconfig/BaseConfig.java
+++ src/main/java/org/grizz/keeper/springconfig/BaseConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Configuration
 @Import(SecurityConfig.class)
 public class BaseConfig {
@@ -20,9 +22,15 @@ public class BaseConfig {
 
     @Bean
     public CommonsRequestLoggingFilter requestLoggingFilter() {
-        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter() {
+            @Override
+            protected void afterRequest(HttpServletRequest request, String message) {
+                // ignore
+            }
+        };
         loggingFilter.setIncludeClientInfo(true);
         loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setBeforeMessagePrefix("Processing request [");
         return loggingFilter;
     }
 }
